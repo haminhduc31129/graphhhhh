5e0490598b7111fc7b57be7e4e41e26da1471c6e
H M D <haminhduc31129@gmail.com>
2026-04-05 21:13:15 +0700

    hoan thien san pham

--- mai.py
+++
@@ -1,5 +0,0 @@
-u = [0]*(m+1)
-v = [0]*(m+1)
-
-for i in range(1, m+1):
-    u[i], v[i] = map(int, input().split()) 
--- main.cpp
+++
@@ -1,5 +0,0 @@
-int u[MAXM], v[MAXM];
-
-for (int i = 1; i <= m; i++) {
-    cin >> u[i] >> v[i];
-} 
